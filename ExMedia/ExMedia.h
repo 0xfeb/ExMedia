8ba2592d7e7546e97a046ccc565ad55d611411db
王渊鸥 <wangyuanou@foxmail.com>
2018-07-27 21:02:31 +0800

    Initial Commit

---
+++ ExMedia/ExMedia.h
@@ -0,0 +1,19 @@
+//
+//  ExMedia.h
+//  ExMedia
+//
+//  Created by 王渊鸥 on 2018/7/27.
+//  Copyright © 2018 WangYuanOu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExMedia.
+FOUNDATION_EXPORT double ExMediaVersionNumber;
+
+//! Project version string for ExMedia.
+FOUNDATION_EXPORT const unsigned char ExMediaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExMedia/PublicHeader.h>
+
+
